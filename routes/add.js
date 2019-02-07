fff7e0a3a8efa721d4529c3e9c463f9e693c32a4
annsudhart <sudharta.andrea@gmail.com>
2019-02-07 10:58:10 -0800

    add working

--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,14 @@
 var data = require("../data.json");
 
-exports.addFriend = function(request, response) {    
+exports.addFriend = function(request, response) {
 	// Your code goes here
- }
+  var newName = request.query.name;
+  var newDescription = request.query.description;
+  var newFriend = {
+    'name': newName,
+    'description': newDescription,
+    'imageURL': "http://lorempixel.com/500/500/people"
+  }
+  data.friends.push(newFriend);
+  console.log(newFriend);
+}
